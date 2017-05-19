bb4b0b938c68c57fb28ddfb377cd925de76fb7a4
hung-le <hung-le@users.noreply.github.com>
2017-05-19 16:47:52 -0700

    Add to repo.

---
+++ src/main/java/com/hungle/example/springWro4J/SpringWro4JApplication.java
@@ -0,0 +1,12 @@
+package com.hungle.example.springWro4J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWro4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWro4JApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hungle/example/springWro4J/SpringWro4JApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hungle.example.springWro4J;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWro4JApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
